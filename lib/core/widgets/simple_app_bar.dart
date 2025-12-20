ef2f924ef6c6eeb5eb6e7a9120999232aa33f427
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-20 21:09:53 +0200

    Create simple_app_bar.dart

---
+++ lib/core/widgets/simple_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:eyego_task/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SimpleAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String? title;
+  const SimpleAppBar({super.key, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      leading: IconButton(
+        onPressed: () {
+          GoRouter.of(context).pop();
+        },
+        icon: Icon(Icons.arrow_back_ios_new),
+      ),
+      title: title != null
+          ? Text(
+              title!,
+              style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+            )
+          : null,
+      centerTitle: true,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
