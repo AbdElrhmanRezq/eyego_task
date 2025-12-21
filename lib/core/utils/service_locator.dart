753926e6c4fedd958d8159a5b8da8de0bd55420a
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-21 19:32:17 +0200

    Added dependency injection using getIt

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,1 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
 
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+}
