08f99b53a674ed671f196261aa1a38f1779a9152
AbdElrhman Ahmed <87246611+AbdElrhmanRezq@users.noreply.github.com>
2025-12-21 22:48:33 +0200

    Created the article model

---
+++ lib/features/home/data/models/article_model.dart
@@ -0,0 +1,64 @@
+class ArticleModel {
+  final Source? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final DateTime? publishedAt;
+  final String? content;
+
+  ArticleModel({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      source: json['source'] != null ? Source.fromJson(json['source']) : null,
+      author: json['author'] as String?,
+      title: json['title'] as String?,
+      description: json['description'] as String?,
+      url: json['url'] as String?,
+      urlToImage: json['urlToImage'] as String?,
+      publishedAt: json['publishedAt'] != null
+          ? DateTime.parse(json['publishedAt'])
+          : null,
+      content: json['content'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'source': source?.toJson(),
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'urlToImage': urlToImage,
+      'publishedAt': publishedAt?.toIso8601String(),
+      'content': content,
+    };
+  }
+}
+
+class Source {
+  final String? id;
+  final String? name;
+
+  Source({this.id, this.name});
+
+  factory Source.fromJson(Map<String, dynamic> json) {
+    return Source(id: json['id'] as String?, name: json['name'] as String?);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name};
+  }
+}
